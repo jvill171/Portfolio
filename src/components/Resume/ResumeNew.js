711b1ab755f62943f9215c6413eaf45b0e843e18
jvill171 <juanvillarreal64@yahoo.com>
2024-07-27 20:29:55 -0500

    Updated resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/JV_[FS]_5_9_2024.pdf";
+import pdf from "../../Assets/../Assets/Resume [Portfolio].pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
