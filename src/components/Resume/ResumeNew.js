4d1a3f490f57e9cc3de71e8d3b6769a31a60b7d9
jvill171 <juanvillarreal64@yahoo.com>
2024-07-27 21:36:08 -0500

    Fixed react-pdf not showing both pages properly

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -32,9 +32,14 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+            {Array.from(new Array(2), (_, index) => (
+              <>
+                {index > 0 ? <p></p> : ""}
+                <Document file={pdf} className="d-flex justify-content-center">
+                  <Page key={`page_${index + 1}`} pageNumber={index + 1} scale={width > 786 ? 1.7 : 0.6} />
+                </Document>
+              </>
+            ))}
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
